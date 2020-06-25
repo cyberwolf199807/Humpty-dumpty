acabbf384918becb5927d78ba5576d99acc5f48b
Prakhar <fletchertandon@gmail.com>
2020-06-25 19:59:34 +0530

    changed

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express=require('express');
 const app=express();
+app.get('/first',(req,res)=>res.end("Prakhar"));
 app.use(express.static('Public'));
 app.listen(process.env.PORT || 5001,()=>console.log("Working"));
