e1bf75211953614214f1962ba12225b8099d180c
Prakhar <fletchertandon@gmail.com>
2020-06-25 20:08:25 +0530

    mocha

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express=require('express');
 const app=express();
-app.get('/first',(req,res)=>res.end("Prakhar"));
+app.get('/first',(req,res)=>res.sendFile(__dirname+'/Public/indo1.html'));
 app.use(express.static('Public'));
 app.listen(process.env.PORT || 5001,()=>console.log("Working"));
