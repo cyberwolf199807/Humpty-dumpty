4000258682a3fe9c63141cd0713c1e38d3e54420
Prakhar <fletchertandon@gmail.com>
2020-06-25 20:33:48 +0530

    Added

--- server.js
+++ server.js
@@ -1,5 +1,9 @@
 const express=require('express');
+const router=require('./routes/routeposts.js');
+const router1=require('./middleware.js');
 const app=express();
-app.get('/first',(req,res)=>res.sendFile(__dirname+'/Public/indo1.html'));
+const middle=(req,res,next)=>{console.log("request received for"+req.url);next();};
+app.use(middle);
+app.use(router1.route);
 app.use(express.static('Public'));
 app.listen(process.env.PORT || 5001,()=>console.log("Working"));
